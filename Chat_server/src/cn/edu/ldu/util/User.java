0838fa9bc5f5bd6e9be665c00ad9befb3192a477
WangYuxing0924 <96657266+WangYuxing0924@users.noreply.github.com>
2022-11-28 19:37:10 +0800

    Update User.java

--- Chat_server/src/cn/edu/ldu/util/User.java
+++ Chat_server/src/cn/edu/ldu/util/User.java
@@ -2,10 +2,7 @@ package cn.edu.ldu.util;
 
 import java.net.DatagramPacket;
 
-/**
- * User类，定义用户对象，包含用户名和收到的报文
- * @author 董相志，版权所有2016--2018，upsunny2008@163.com
- */
+
 public class User {
     private String userId=null; //用户id
     private DatagramPacket packet=null; //报文
